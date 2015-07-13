e502563a60f64b3f21673537b07fefdf766f3d39
Per Ivar Nerseth <perivar@nerseth.com>
2015-07-14 00:59:02 +0200

    Added a TimerUtil class

---
+++ Library/Source/TimerUtils.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace CommonUtils
+{
+	/// <summary>
+	/// A simple timer class
+	/// </summary>
+	public class TimerUtil
+	{
+		private readonly Stopwatch _watch;
+
+		/// <summary>
+		/// Create a simple timer class
+		/// <example>
+		/// var timer = new TimerUtil();
+		/// Thread.Sleep(10000);
+		/// var elapsed = timer.Stop();
+		/// </example>
+		/// </summary>
+		public TimerUtil() {
+			_watch = Stopwatch.StartNew();
+		}
+
+		public TimeSpan Stop()
+		{
+			_watch.Stop();
+			
+			// Get the elapsed time as a TimeSpan value.
+			return _watch.Elapsed;
+		}
+	}
+}
